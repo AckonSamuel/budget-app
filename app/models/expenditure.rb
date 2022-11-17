85e38e00bab5a28eb677a1eb02488c1e72c2ffda
Samuel Ackon <ackons433@gmail.com>
2022-11-17 21:17:49 +0000

    Add model methods to group class model

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,2 +1,3 @@
 class Expenditure < ApplicationRecord
+    
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,20 @@
 class Group < ApplicationRecord
+    belongs_to :users
+    has_many :group_expenditures
+    has_many :expenditures, through: :group_expenditures
+
+    validates :name, presence: true
+    validates :icon, presence: true
+
+    def recent_expenditures
+        expenditures.order(created_at: :desc)        
+    end
+
+    def total_expenditures
+        expenditures.sum(&:amount)
+    end
+
+    def expenditures_count
+        expenditures.count
+    end
 end
