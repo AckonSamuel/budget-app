ca3086407b47d6ca905b0811de6f1fc25fcc9952
Samuel Ackon <ackons433@gmail.com>
2022-11-17 21:29:31 +0000

    Create group controller methods

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,11 +3,12 @@ class GroupsController < ApplicationController
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = current_user.groups
   end
 
   # GET /groups/1 or /groups/1.json
   def show
+    @group = Group.new
   end
 
   # GET /groups/new
@@ -21,50 +22,18 @@ class GroupsController < ApplicationController
 
   # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
+    group = Group.new(group_params)
+    group.user = current_user
 
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to group_url(@group), notice: "Group was successfully created." }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /groups/1 or /groups/1.json
-  def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: "Group was successfully updated." }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /groups/1 or /groups/1.json
-  def destroy
-    @group.destroy
-
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
-      format.json { head :no_content }
+    if group.save
+      redirect_to groups_path, notice: 'Group successfully create'
+    else
+      redirect_to new_group_path, notice: 'Group not created'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def group_params
-      params.fetch(:group, {})
-    end
+  def group_params
+    params.require(:new_group).permit(:name, :icon)
+  end
 end
--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,3 +1,8 @@
 class Expenditure < ApplicationRecord
-    
+    belongs_to :users
+    has_many :group_expenditures
+    has_many :groups, through: :group_expenditures
+
+    validates :name, presence: true
+    validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
